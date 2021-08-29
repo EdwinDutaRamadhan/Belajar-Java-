20923acf37e93016e8c60e09e6bcfe1a710f3fc7
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2021-08-29 23:43:04 +0700

    belajar java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.tugas;
+
+public class Main {
+    //System.out.println("");
+    public static void main(String[] args) {
+
+    }
+}
---
+++ dataMahasiswa.java
@@ -0,0 +1,18 @@
+package com.tugas;
+import java.util.Scanner;
+public class dataMahasiswa {
+    String[] nimMahasiswa = new String[10];
+    String[] namaMahsiswa = new String[30];
+    String[] asalMahasiswa = new String[15];
+    int pilihan = 0;
+    void inputDataMahasiswa(){
+        while(pilihan != 4){
+            Scanner data = new Scanner(System.in);
+            System.out.println("NIM :");
+            nimMahasiswa[i] = data.nextLine();
+            System.out.println("Nama :");
+            System.out.println("Asal : ");
+        }
+    }
+
+}
